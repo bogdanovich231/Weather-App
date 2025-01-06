665dbb71970bf186785382c0acc1a832badeb819
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-01-07 00:42:48 +0100

    feat: request to api

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -1,14 +1,26 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_URL = process.env.API_URL; 
+const API_KEY = process.env.API_KEY; 
 
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
   endpoints: (builder) => ({
+    getWeather: builder.query({
+      query: (city) => ({
+        url: `weather`, 
+        params: {
+          q: city, 
+          appid: API_KEY, 
+          units: 'metric', 
+          lang: 'pl', 
+        },
+      }),
+    }),
   }),
 });
 
 export const {
-
+useGetWeatherQuery
 } = api;
