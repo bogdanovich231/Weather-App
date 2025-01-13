10cf37da7a2bd942cc299f2e6928c490fea2f9e1
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-01-13 22:52:32 +0100

    feat: add opening the weather of favorite city

--- src/Components/FavoriteCities/FavoriteCities.tsx
+++ src/Components/FavoriteCities/FavoriteCities.tsx
@@ -4,10 +4,12 @@ import { RootState } from '../../store/store';
 import { useDispatch, useSelector } from 'react-redux';
 import IconFavoriteFill from '../../assets/favoriteFill.svg';
 import { removeFavorite } from '../../store/slices/favorite.slice';
+import { useNavigate } from 'react-router-dom';
 
 function FavoriteCities() {
   const dispatch = useDispatch();
   const { cities: favoriteCities } = useSelector((state: RootState) => state.favorites);
+  const navigate = useNavigate();
 
   const toggleFavorite = (cityName: string) => {
     if (favoriteCities.includes(cityName)) {
@@ -15,12 +17,16 @@ function FavoriteCities() {
     }
   };
 
+  const handleCityClick = (cityName: string) => {
+    navigate(`/forecast/${cityName}`);
+  };
+
   return (
     <div className={styles.containerFavoriteCities}>
       {favoriteCities.length > 0 ? (
         favoriteCities.map((city) => (
           <div key={city} className={styles.wrapperCity}>
-            <div className={styles.informationCity}>
+            <div className={styles.informationCity} onClick={() => handleCityClick(city)}>
               <img src={IconRainy} alt="Weather Icon" />
               <h3>{city}</h3>
             </div>
